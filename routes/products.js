66055509be4324ee3f232202130c77c2c7e36e7a
Ayush <sameera.r@digiapt.com>
2018-10-02 18:46:29 +0530

    change name to appropriate

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var usersController = require("../controllers/products")
+var productsController = require("../controllers/products")
 
 /* GET products listing. */
-router.get('/', usersController.getProducts);
+router.get('/', productsController.getProducts);
+
+/* POST create product. */
+
 
 module.exports = router;
