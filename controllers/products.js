56a4e0b7d9a88a1a020b4a773a693dea6ce3b54f
Ayush <sameera.r@digiapt.com>
2018-10-02 19:39:10 +0530

    established db connection for get products API

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var db = require('../db/db');
 
+var dbConnection;
+
 exports.getProducts = function (req, res) {
     console.log("entering into getProducts");
     var users;
@@ -12,10 +14,24 @@ exports.getProducts = function (req, res) {
         name:"iPhone X",
         age:"30000 INR"
     }]
-    console.log("exiting from getProducts");
-    res.status(200).send(users);
-}
+    
+    dbConnection = db.getDbConnection();
+    
+    var queryStatement = "select product_Name, price from products";
 
+    console.log("query to be exectuted:: ",queryStatement);
+
+    dbConnection.query(queryStatement,function(err,result){
+		if(err) {
+			console.log("error: ",err);
+			res.status(500).send(err);		
+		} else {
+            console.log("success: ",result);
+            res.status(200).send(result);
+        }
+        console.log("exiting from getProducts");
+    });
+}
 
 // create product
 exports.createProduct = function(req, res) {
@@ -25,7 +41,7 @@ exports.createProduct = function(req, res) {
     // print inputs
     console.log("request body: ", req.body);
 
-    var dbConnection = db.getDbConnection();
+    dbConnection = db.getDbConnection();
     var queryStatement = "insert into products values('"+req.body.product_id+"','"+req.body.product_Name+"',"+req.body.price+",now(),now(),"+req.body.quentity+")";
 
     console.log("query to be exectuted:: ",queryStatement);
